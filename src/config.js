e84ff365de73e9cdc022fea1ea96473d611e89c6
Nicola Masotti <nicolamasotti@hotmail.it>
2022-11-16 16:25:37 +0100

    Add Aridity Actual indicator

--- src/config.js
+++ src/config.js
@@ -14,11 +14,11 @@ const config = {
     toolbox_app:
       "https://cds.climate.copernicus.eu/workflows/c3s/%APP%/master/configuration.json",
     git_md:
-      "https://raw.githubusercontent.com/bopen/c3s_430a_ecde_page_text/splitted-ConsolidatedText/content/markdown/consolidated/%TITLE%.md",
+      "https://raw.githubusercontent.com/bopen/c3s_430a_ecde_page_text/feature-aridity-actual-index/content/markdown/consolidated/%TITLE%.md",
     git_json:
-      "https://raw.githubusercontent.com/bopen/c3s_430a_ecde_page_text/splitted-ConsolidatedText/content/json/Consolidated_430a.json",
+      "https://raw.githubusercontent.com/bopen/c3s_430a_ecde_page_text/feature-aridity-actual-index/content/json/Consolidated_430a.json",
     git_glossary_json:
-      "https://raw.githubusercontent.com/bopen/c3s_430a_ecde_page_text/splitted-ConsolidatedText/content/json/Glossary.json",
+      "https://raw.githubusercontent.com/bopen/c3s_430a_ecde_page_text/feature-aridity-actual-index/content/json/Glossary.json",
   },
   usage: {
     markdown_texts: false,
