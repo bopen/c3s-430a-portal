68a06a53542f5bdd976769d22655ab428acb17f1
MVIvien <v.mavel@bopen.eu>
2023-06-16 12:53:05 +0200

    add script to generate overview_table.json

---
+++ gsheet_parser/overview_table.py
@@ -0,0 +1,38 @@
+import json
+
+
+def make_overview_table():
+    with open(f'../data/data_consolidated.json') as f:
+        data = json.load(f)
+
+    overview_table = {}
+    for key, indicator in data['indicators'].items():
+        hazard_category = indicator['hazard_category']
+        hazard_type = indicator['hazards'][0]
+        theme = indicator['theme'][0].lower()
+        slug = indicator['page_url']
+        
+        new_item = {
+            'indicator_text': indicator['page_title'],
+            'indicator_url': f'{theme}/{slug}.html',
+            'zip_text': 'Zip download',
+            'zip_url': 'pippo'
+        }
+
+        if overview_table.get(hazard_category) is None:
+            overview_table[hazard_category] = {
+                hazard_type: []
+            }
+        if overview_table[hazard_category].get(hazard_type) is None:
+            overview_table[hazard_category][hazard_type] = []
+                
+        overview_table[hazard_category][hazard_type].append(new_item)
+
+    with open(f'../data/overview_table.json', 'w', encoding='utf-8') as f:
+        json.dump(overview_table, f, ensure_ascii=False, indent=4)
+
+
+def make_html_table_content
+
+if __name__ == '__main__':
+    make_overview_table()
